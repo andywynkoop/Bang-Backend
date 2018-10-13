c5b11204937052a04e4ef41c73e686590fcd279f
Andy Wynkoop <andywynkoop@gmail.com>
2018-10-12 20:17:38 -0700

    Remove biscuit requirement

--- middlewares/biscuits.js
+++ middlewares/biscuits.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
   let { data } = req.query;
+  if (!data) next();
   if (data) data = JSON.parse(data);
   const biscuits = data.biscuits ? data.biscuits : {};
   Object.assign(req.parms, data);
